a0712b199f427aa5a197ed12c409ec9dace8d2f6
Alex Hosszu <ahosszu@airtouchmedia.com>
2023-07-28 11:32:14 +0300

    wip

--- app/src/main/java/com/example/blixtest/MainActivity.kt
+++ app/src/main/java/com/example/blixtest/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.blixtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.example.blixtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragmentContainerView) as NavHostFragment?
+        val navController = navHostFragment?.navController
+
+
     }
 }
